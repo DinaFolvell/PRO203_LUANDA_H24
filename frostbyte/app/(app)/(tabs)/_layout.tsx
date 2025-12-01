30d3b9e568e5927039a0a134b12dca726382655a
DinaFolvell <dimafofo@hotmail.no>
2025-12-01 14:08:35 +0100

    added home icon

--- frostbyte/app/(app)/(tabs)/_layout.tsx
+++ frostbyte/app/(app)/(tabs)/_layout.tsx
@@ -1,21 +1,21 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
 import { Tabs } from "expo-router";
+import React from "react";
 
-
-export default function TabBar(){
-return (
-  <Tabs>
-    {/* 
-    for eks:
-    <Tabs.Screen
-        name="index"
-        options={{
-          title: "Hjem",
-           tabBarIcon: ({ color }) => (
-            <AntDesign name="home" size={24} color={color} />
-          ),
-        }}
-      />
-*/}
-  </Tabs>
-);
+export default function TabBar() {
+  return (
+    <Tabs>
+      {
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Hjem",
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="home" size={24} color={color} />
+            ),
+          }}
+        />
+      }
+    </Tabs>
+  );
 }
