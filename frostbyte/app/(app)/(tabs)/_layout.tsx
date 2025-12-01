65ace952a23109ae3529621fb511825a1061d2ea
DinaFolvell <dimafofo@hotmail.no>
2025-12-01 13:46:15 +0100

    file structure

---
+++ frostbyte/app/(app)/(tabs)/_layout.tsx
@@ -0,0 +1,21 @@
+import { Tabs } from "expo-router";
+
+
+export default function TabBar(){
+return (
+  <Tabs>
+    {/* 
+    for eks:
+    <Tabs.Screen
+        name="index"
+        options={{
+          title: "Hjem",
+           tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={24} color={color} />
+          ),
+        }}
+      />
+*/}
+  </Tabs>
+);
+}
--- frostbyte/app/(app)/(tabs)/index.tsx
+++ frostbyte/app/(app)/(tabs)/index.tsx
@@ -1,5 +1,10 @@
 import { Text, View } from "react-native";
 
+//Inne i (tabs) har vi alle sidene
+//som dashboard, checkout
+//Her har vi også en _layout fil som kan inneholde en tabBar for å navigere mellom
+// filene i (tabs)
+
 export default function Index() {
   return (
     <View
