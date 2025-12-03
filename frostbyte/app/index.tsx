eabdb3becdc89dfe6584e00d6d33f2f0a42d9ab5
DinaFolvell <dimafofo@hotmail.no>
2025-12-03 09:46:00 +0100

    added components to index

--- frostbyte/app/index.tsx
+++ frostbyte/app/index.tsx
@@ -1,17 +1,27 @@
-// app/index.tsx
-import { StatusBar } from '../components/staus-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AttendanceButton } from "@/components/attendance-button";
+import { StatusBar } from "../components/staus-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AbsenceButton } from "@/components/absence-button";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is the home screen</Text>
+      <View style={styles.buttonContainer}>
+        <AttendanceButton />
+        <AbsenceButton />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
   text: { fontSize: 20 },
+
+  buttonContainer: {
+    flexDirection: "row",
+    gap: 16, 
+    marginTop: 20,
+  },
 });
