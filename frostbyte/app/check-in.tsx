65bd559cceea8f6f51e2a81e1e4d4c81ccfcc268
Martinesunde <martine-sun@hotmail.com>
2025-12-08 10:49:22 +0100

    addded attendance-card.tsx

--- frostbyte/app/check-in.tsx
+++ frostbyte/app/check-in.tsx
@@ -1,3 +1,4 @@
+import AttendanceCard from '@/components/attendance-card';
 import { AttendanceOverview } from '@/components/attendance-overview';
 import AttendanceDropdown from '@/components/dropdown-menu';
 import React from 'react';
@@ -7,7 +8,12 @@ export default function CheckInScreen() {
   return (
     <View style={styles.container}>
       <AttendanceOverview style={styles.overView} />
-      <AttendanceDropdown/>
+      <AttendanceDropdown />
+      <AttendanceCard
+        photoUrl={require("../assets/images/dina.jpg")}
+        name="Amalie S. Ulriksen"
+        note="Blir hentet klokka 1200 pga. tidlig ferie"
+      />
     </View>
   );
 }
