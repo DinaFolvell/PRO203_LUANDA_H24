e65deef5cdfb35db1ae50a343f3922e3d9283f79
Sigurd Jongers <sijo063@student.kristiania.no>
2025-12-10 11:56:45 +0100

    added add button

--- frostbyte/app/absence.tsx
+++ frostbyte/app/absence.tsx
@@ -1,11 +1,19 @@
-import { HeaderBar } from "@/components/header-bar";
+import { AddButton } from "@/components/absence/add-button";
+import { HeaderBar } from "@/components/absence/header-bar";
 import { View } from "react-native";
 
 export default function AbsenceScreen() {
     
   return (
     <View >
-     <HeaderBar />
+    <HeaderBar
+        onPrevWeek={() => console.log('Previous week clicked')}
+        onNextWeek={() => console.log('Next week clicked')}
+        onNotifications={() => console.log('Notification bell clicked')}
+    />
+
+     <AddButton onPress={() => console.log('Add button clicked')} />
+
     </View>
   );
 }
