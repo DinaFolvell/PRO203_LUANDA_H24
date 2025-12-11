86a58cb3b7038b746fb210c3ec8a115125f64788
DinaFolvell <dimafofo@hotmail.no>
2025-12-11 11:59:59 +0100

    firebase auth + login

--- frostbyte/app/login.tsx
+++ frostbyte/app/login.tsx
@@ -1,62 +1,73 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity} from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { signIn } from "@/api/authApi";
+import { router } from "expo-router";
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
+    try {
+      const user = await signIn(username, password);
 
-    console.log('Login attempt with:', { username, password });
+      if (user) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log("Login failed:", error);
+    }
   };
 
   return (
-    
-      <View style={styles.loginContainer}>
-        <Text style={styles.title}>Bapa - ansatt</Text>
-        <Text style={styles.subtitle}>Logg in med din bruker-ID</Text>
+    <View style={styles.loginContainer}>
+      <Text style={styles.title}>Bapa - ansatt</Text>
+      <Text style={styles.subtitle}>Logg inn med din bruker-ID</Text>
+      <Text >Brukernavn: 123@mail.no</Text>
+      <Text >Passord: passord123</Text>
 
-        <TextInput
-          style={styles.input}
-          placeholder="Bruker-ID"
-          placeholderTextColor="#999"
-          value={username}
-          onChangeText={setUsername}//Vet ikke hvilke funksjoner som trengs her
-          autoCapitalize="none"
-          autoCorrect={false}
-        />
+      <TextInput
+        style={styles.input}
+        placeholder="Bruker-ID"
+        value={username}
+        onChangeText={setUsername}
+        autoCapitalize="none"
+      />
 
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          placeholderTextColor="#999"
-          value={password}
-          onChangeText={setPassword} //Vet ikke hvilke funksjoner som trengs her
-          secureTextEntry
-          autoCapitalize="none"
-        />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
 
-        <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-      </View>
-    
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
   },
   loginContainer: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingHorizontal: 30,
   },
   title: {
     fontSize: 32,
-    color: '#f54500',
+    color: "#f54500",
     marginBottom: 8,
     textAlign: "center",
     fontStyle: "normal",
@@ -64,30 +75,30 @@ const styles = StyleSheet.create({
   },
   subtitle: {
     fontSize: 16,
-    color: '#666',
+    color: "#666",
     marginBottom: 40,
     textAlign: "center",
   },
   input: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingHorizontal: 15,
     paddingVertical: 12,
     borderRadius: 8,
     fontSize: 16,
     marginBottom: 15,
     borderWidth: 1,
-    borderColor: '#ddd',
+    borderColor: "#ddd",
   },
   button: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingVertical: 15,
     borderRadius: 8,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
   },
   buttonText: {
-    color: '#f54500',
+    color: "#f54500",
     fontSize: 16,
-    fontWeight: '600',
+    fontWeight: "600",
   },
 });
