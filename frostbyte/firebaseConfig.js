e0fa78e2140f4023f86a595a25d893fc4e6040ec
DinaFolvell <dimafofo@hotmail.no>
2025-12-11 10:56:46 +0100

    bug fix auth

--- frostbyte/firebaseConfig.js
+++ frostbyte/firebaseConfig.js
@@ -1,29 +1,38 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import firebaseConfig from "./firebaseEnv";
 import { getFirestore } from "firebase/firestore";
 import {
   initializeAuth,
+  getAuth,
   getReactNativePersistence,
   browserLocalPersistence,
 } from "firebase/auth";
 import { Platform } from "react-native";
 
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 let persistence;
 
-// Use browser persistence on web
+
 if (Platform.OS === "web") {
   persistence = browserLocalPersistence;
 } else {
-  // Load only on real mobile platforms
   const AsyncStorage =
     require("@react-native-async-storage/async-storage").default;
+
   persistence = getReactNativePersistence(AsyncStorage);
 }
 
-// Initialize auth with correct persistence
-export const auth = initializeAuth(app, { persistence });
+let auth;
+
+
+if (!getApps()[0]._auth) {
+  auth = initializeAuth(app, { persistence });
+} else {
+  auth = getAuth(app); 
+}
+
+export { auth };
 
 // Firestore
 export const db = getFirestore(app);
