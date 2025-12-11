cb4a396d6aa1b3a83aa20f24f3a9a998f5909cb4
Sigurd Jongers <sijo063@student.kristiania.no>
2025-12-11 10:01:54 +0100

    date select

--- frostbyte/app/add-absence.tsx
+++ frostbyte/app/add-absence.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { ChildSelect } from '@/components/absence/child-select';
+import { DateSelect } from '@/components/absence/date-select';
 
 export default function AddAbsenceScreen() {
     return (
-        <View>
+        <View style={styles.container}>
             <ChildSelect />
+            <DateSelect />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 32,
+        alignItems: 'center',
+    },
+});
